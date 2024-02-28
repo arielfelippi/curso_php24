81a111f29b9ecb9a6520248514a2de2c3f740c3d
arielfelippi <ariel.felippi@hotmail.com>
2024-02-27 21:29:40 -0300

    exercicios tema de casa

--- exercicios.php
+++ exercicios.php
@@ -12,11 +12,40 @@ $i=0;
 while ($i <= 10) {
     $tabuada = $num * $i;
 
-    if ($i == 10) {
-        echo "{$num} x {$i} = {$tabuada}\n";
-    } else {
-        echo "{$num} x {$i} = {$tabuada},\n";
-    }
+    // if ($i == 10) {
+    //     echo "{$num} x {$i} = {$tabuada}\n";
+    // } else {
+    //     echo "{$num} x {$i} = {$tabuada},\n";
+    // }
 
     $i++;
 }
+
+/*
+$c = celsius
+$k = kelvin
+$f = farenheit
+*/
+$tipoTemperatura = 'c';
+$c = 16;
+
+if ($tipoTemperatura == 'c')  {
+    $f = $c * 1.8 + 32;
+    $k = $c + 273;
+    echo "A temperatura em farenheit é: {$f} e, a temperatura em Kelvin é: {$k}." ;
+} else if ($tipoTemperatura == 'f')  {
+    $c = $f * 1.8 - 32;
+    $k = $f + 273;
+    echo "A temperatura em farenheit é: {$c} e, a temperatura em Kelvin é: {$k}." ;
+} else if ($tipoTemperatura == 'k')  {
+    $f = $k * 1.8 + 32;
+    $c = $k + 273;
+    echo "A temperatura em farenheit é: {$f} e, a temperatura em Kelvin é: {$c}." ;
+}
+
+/*
+    calcular o IMC
+*/
+
+
+
