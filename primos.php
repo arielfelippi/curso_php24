8a3b1a9c1df6ac9b720327f4377729951517a6ab
arielfelippi <41402358+arielfelippi@users.noreply.github.com>
2024-04-09 21:32:50 -0300

    Create primos.php
    calcular os numeros primos

---
+++ primos.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+    Calcular os 5 primeiros numeros primos.
+    Saída esperada: 2, 3, 5, 7, 11.
+    Regra: só é divisivel por 1 e por ele mesmo.
+    Ex.: 7 => 7/1 e 7/7.
+    Dica: Utilizar o operador de módulo % para pegar o resto da divisão.
+*/
+
