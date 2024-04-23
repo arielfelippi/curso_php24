6901e546a3bc7eb01edd8383ef4b6f67c59d3ebe
arielfelippi <41402358+arielfelippi@users.noreply.github.com>
2024-04-23 19:37:39 -0300

    Update primos.php

--- primos.php
+++ primos.php
@@ -8,3 +8,27 @@
     Dica: Utilizar o operador de módulo % para pegar o resto da divisão.
 */
 
+$primos = [];
+
+for($numero = 2; count($primos) < 5; $numero++) {
+    $isPrimo = true;
+
+    for($divisor = 2; $divisor < $numero; $divisor++) {
+        $resto = $numero % $divisor;
+
+        if ($resto == 0) {
+            $isPrimo = false;
+            break;
+        }
+    }
+
+    if ($isPrimo) {
+        $primos[] = $numero;
+    }
+}
+
+echo "Os números primos são: <br>";
+
+for($i = 0; $i < count($primos); $i++) {
+    echo $primos[$i] . " " ;
+}
